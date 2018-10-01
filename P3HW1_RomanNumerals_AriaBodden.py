bbaf2f31c362b42696f7e0d4b31d394ac0db0cb2
albodden <43594348+albodden@users.noreply.github.com>
2018-10-01 12:47:36 -0400

    Roman Numerals

---
+++ P3HW1_RomanNumerals_AriaBodden.py
@@ -0,0 +1,41 @@
+#Convert regular number to Roman numerals
+#CTI-110
+#P3HW1 - Roman Numerals
+#Aria Bodden
+#10/01/18
+
+#ask user to enter a number between 1 and 10
+
+number = int(input("Please enter a number between 1 and 10: "))
+
+#set statements to display corresponding Roman numerals
+
+if number == 1:
+    print("I")
+elif number == 2:
+    print("II")
+elif number == 3:
+    print("III")
+elif number == 4:
+    print("IV")
+elif number == 5:
+    print("V")
+elif number == 6:
+    print("VI")
+elif number == 7:
+    print("VII")
+elif number == 8:
+    print("VIII")
+elif number == 9:
+    print("IX")
+elif number == 10:
+    print("X")
+else:
+    print("Error! The number entered must lie between 1 and 10.")
+    
+    
+
+    
+
+
+
